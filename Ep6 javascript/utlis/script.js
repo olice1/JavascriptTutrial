3a49d2969c9450b7e9336a6a86c18869d8284d43
Olice1 <mag6man@gmail.com>
2023-11-21 23:35:53 +0100

    Rest of tutrial of HTML/CSS/JAVASCRIPT ep5 & 6

---
+++ Ep6 javascript/utlis/script.js
@@ -0,0 +1,90 @@
+
+var Age = 35 +2;
+let Persone = 'Hamid'
+var educated = false
+Persone = 'Ahmed'
+
+console.log('this persone is : '+ Persone + ' and his Age: ' + Age + ' is educated ' + educated)
+
+
+const p = 3.14
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  first Line in javascript => Hello world!
+//  print it on console 
+
+
+
+
+//  variables
+// Number	Double precision floating-point
+// String	Series of characters
+// Boolean	true or false
+// Array	Ordered sequence of values
+// Value	String, Number, Boolean, null etc
+// Object	Unordered collection of key/value pairs
+// null	Null or Empty
+
+
+
+
+
+
+
+//Arithmetic Operators
+// 5 + 5 = 10     // Addition
+// 10 - 5 = 5     // Subtraction
+// 5 * 10 = 50    // Multiplication
+// 10 / 5 = 2     // Division
+// 10 % 5 = 0     // Modulo
+
+//[1, 2, 3, 4, 5]
